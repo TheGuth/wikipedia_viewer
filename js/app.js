854f3bfe3edd132e751488a1acc641b00d0f4b50
Jean-Luc19 <aticus19@gmail.com>
2017-02-26 13:32:25 -0700

    Introduces API calls to wikipedia

--- js/app.js
+++ js/app.js
@@ -1,10 +1,32 @@
-const wikipediaUrl = "https://en.wikipedia.org/w/api.php?action=query&titles=Albert%20Einstein&prop=info&format=jsonfm "
-
-// https://en.wikipedia.org/w/api.php?action=query&generator=allpages&gaplimit=10&gapfrom=World&prop=links|categories
+const wikipediaUrl = "https://en.wikipedia.org/w/api.php?callback=?";
+// const wikipediaUrl =https://en.wikipedia.org/w/api.php?action=query&generator=allpages&gaplimit=10&gapfrom=World&prop=links|categories
+const appState = {
+    pages: [],
+};
 
 const query = {
   format: "json",
   action: "query",
   titles: "",
-  prop:
+  generator: "allpages",
+  gaplimit: 10,
+  prop: "info|",
 }
+
+function searchWiki(item, callback) {
+  query.gapfrom = item;
+  $.getJSON(wikipediaUrl, query, callback)
+};
+
+function wikiCallback (data) {
+  const idKeys = Object.keys(data.query.pages);
+  idKeys.forEach( id => {
+    appState.pages.push(data.query.pages[id]);
+  });
+  console.log(appState.pages);
+
+};
+
+$(()=> {
+  searchWiki("word", wikiCallback);
+});
