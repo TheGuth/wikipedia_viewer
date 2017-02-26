f6531f372810daaf7f924b321f9ca5b1ca8e208c
James Guthrie <jamesrguthrie@gmail.com>
2017-02-26 14:10:19 -0500

    added wikipedia api endpoint

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const wikipediaUrl = "https://en.wikipedia.org/w/api.php?action=query&titles=Albert%20Einstein&prop=info&format=jsonfm "
+
+// https://en.wikipedia.org/w/api.php?action=query&generator=allpages&gaplimit=10&gapfrom=World&prop=links|categories
+
+const query = {
+  format: "json",
+  action: "query",
+  titles: "",
+  prop:
+}
